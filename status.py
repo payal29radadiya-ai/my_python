5c62ad7c6931328c1f78c91ba032e345202e6603
payal <payal.29radadiya@gmail.com>
2025-11-26 14:29:48 -0500

    status_check_dummy_json

--- status.py
+++ status.py
@@ -20,47 +20,32 @@ def setup_logger(log_file):
 
 #run status command
 
-def get_deployment_status(yaml_file):
-    cmd = ["eac", "deployment", "status", "-f", yaml_file]
-    result = subprocess.run(cmd, capture_output=True, text=True)
-    if result.returncode != 0:
-        raise RuntimeError(f"Status command Failed: {result.stderr}")
-    return result.stdout
-
-#parse status
-
-def parse_status(output)
-    match = re.search(r"Status:\s*(\w+)", output)
-    if match:
-        return match.group(1).lower() #success or failure
-    return None
-
 #main
 def main(yaml_file, log_file):
     logger = setup_logger(log_file)
-    logger.info(f"checking deployment status: {yaml_file}")
+    logger.info(f"Checking deployment status from: {json_file}")
 
     try:
-        output = get_deployment_status(yaml_file)
+        with open(json_file, "r") as f:
+            data = json.load(f)
     except Exception as e:
-        logger.error(f"Failed  to get deployment status: {e}")
-
-    status = parse_status(output)
-
-    if status == "Success":
-        logger.info("Deploymet Status: SUCCESS")
-        sys.exit(0)
-    elif status == "Failure":
-        logger.info("Deploymet Status: FAILURE")
+        logger.error(f"Failed to read JSON file: {e}")
+        print(1)
         sys.exit(1)
+
+    status = str(data.get("Status", "")).lower()
+    if status == "success":
+        logger.info("Deploymet Status: SUCCESS")
+        print(0)
+        sys.exit(0)
     else:
-        logger.info("Could not determine deployment Status from output.")
-        logger.info(output)
+        logger.info("Deploymet Status: FAILURE")
+        print(1)
         sys.exit(1)
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print("Usage: python deployment_status_check.py <deployment.yaml> <deployment_status.log>")
+        print("Usage: python deployment_status_check.py <output.json> <deployment_status.log>")
         sys.exit(1)
 
     main(sys.argv[1], sys.argv[2])
